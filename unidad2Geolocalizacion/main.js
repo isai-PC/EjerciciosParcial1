9df001f2609d6ea87313f1329d4a6cb6e7990fa9
Isaí Pascual Cruz <20241018@uthh.edu.mx>
2026-01-19 08:36:29 -0600

    cambios2

--- unidad2Geolocalizacion/main.js
+++ unidad2Geolocalizacion/main.js
@@ -3,8 +3,9 @@ const enlaceMapa = document.getElementById("enlace");
 
 
 const obtener = () => {
-    //vereficar si el navegador soporta la geolocalizacion
-    if (navigator.geolocation) { //hacer referencia al navegador
+    //vereficar si el navegador soporta la geolocalizacion//hacer referencia al navegador
+    if (navigator.geolocation) { //si esta activa el navegador
+        coordenadas.innerHTML = "Obteniendo coordenadas...";
         navigator.geolocation.getCurrentPosition(mostrarPosicion, mostrarError);
     } else {
         coordenadas.innerHTML = "La geolocalización no es compatible con este navegador.";
