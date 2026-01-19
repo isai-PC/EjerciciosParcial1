eb7d4b56ad1ffb1a81af2638f4ea39774cdf3f5e
Isaí Pascual Cruz <20241018@uthh.edu.mx>
2026-01-19 09:00:21 -0600

    link

--- unidad2Geolocalizacion/main.js
+++ unidad2Geolocalizacion/main.js
@@ -11,14 +11,18 @@ const obtener = () => {
             (posicion) => {
                 const latitud = posicion.coords.latitude;//obtener la
                 const longitud = posicion.coords.longitude;
-                alert(`Latitud: ${latitud} <br> Longitud: ${longitud}`);
-                enlaceMapa.href = `https://www.google.com/maps?q=${latitud},${longitud}`;
-                enlaceMapa.style.display = "block";
+                /* alert(`Latitud: ${latitud} <br> Longitud: ${longitud}`); */
+                coordenadas.innerText = `Latitud: ${latitud}, Longitud: ${longitud}`;
+                enlaceMapa.href = `https://www.google.com/maps?q=${latitud},${longitud}`; //Cambiar el enlace del mapa
+                enlaceMapa.style.display = "block";//cambiar el estilo para que se muestre
+            }
+            (error) => {
+                coordenadas.innerHTML = `No se puedo obtener la informacionn`;
             }
 
 
         );
     } else {
-        coordenadas.innerHTML = "La geolocalización no es compatible con este navegador.";
+        coordenadas.innerHTML ="geolocalización no es compatible con este navegador.";
     }
 };
